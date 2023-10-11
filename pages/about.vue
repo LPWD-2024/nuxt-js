d390790d3e1f1ce829f068b19d029880166bcbdb
mccp <parent.clement2@gmail.com>
2023-10-11 11:42:36 +0200

    feat(pages) : add pages

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-about">
+    Page about
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-index">
+    Page d'accueil
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
